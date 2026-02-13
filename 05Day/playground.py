76d7f8877cd11339be35979395d1693ae2d70781
Shivananda Reddy Kankanala <cyberllama13@gmail.com>
2026-02-13 18:54:15 +1300

    Day 5

---
+++ 05Day/playground.py
@@ -0,0 +1,19 @@
+# f = open("text.txt", "r") # modes - r, w, a, x, b
+# data = f.read()
+# print(data)
+# f.close()
+
+# with open("text.txt", "r") as f:
+#     data = f.read(13)
+#     print(data)
+
+# with open("text.txt") as f:
+#     for line in f:
+#         print(line, end="")
+
+# with open("text.txt", "w") as f:
+#     f.write("line 6")
+
+with open("text.txt", "a") as f:
+    f.write("line 7\n")
+    f.write("line 8\n")
